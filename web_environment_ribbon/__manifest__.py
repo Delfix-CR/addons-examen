a2337630e183f667bdabbfb9857c5e312d562f13
oscardfx <oscar.picado@delfixcr.com>
2022-05-17 10:00:12 -0600

    [ADD] modulos

---
+++ web_environment_ribbon/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2015 Francesco OpenCode Apruzzese <cescoap@gmail.com>
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 Thomas Binsfeld <thomas.binsfeld@acsone.eu>
+# Copyright 2017 Xavier Jiménez <xavier.jimenez@qubiq.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web Environment Ribbon",
+    "version": "19.11.21.1",
+    "category": "Web",
+    "author": "Francesco OpenCode Apruzzese, "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    'assets': {
+        'web.assets_backend': [
+            '/web_environment_ribbon/static/src/css/ribbon.css',
+            '/web_environment_ribbon/static/src/js/ribbon.js',
+        ],
+    },
+    "data": ["data/ribbon_data.xml", "view/base_view.xml"],
+    "auto_install": False,
+    "installable": True,
+}
